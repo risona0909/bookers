055bc0cf751ef350a3175a0c0cb425f051f8eeda
risona0909 <105469316+risona0909@users.noreply.github.com>
2022-06-26 10:57:44 +0000

    [start]アプリケーションを完成させよう課題

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,30 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+     # １.&2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # 4. トップ画面へリダイレクト
+    redirect_to book_path(book.id)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "1fa9b36631b242d5a0c77cd8e81f0eaa.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
